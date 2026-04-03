545c8189bbf2bed5a7466035410b7c36c9a8c44d
Thomas Taulo <taulo@live.dk>
2026-04-03 19:18:54 +0200

    add types

--- kanpla/src/menu.tsx
+++ kanpla/src/menu.tsx
@@ -1,5 +1,6 @@
 import { Detail, LaunchProps } from "@raycast/api";
 import useKanpla from "./hooks/useKanpla";
+import { IMenuItem } from "@taulo1999/kanpla-api";
 
 export default function Command(props: LaunchProps<{ arguments: any }>) {
   const date = props.arguments.date;
@@ -10,9 +11,7 @@ export default function Command(props: LaunchProps<{ arguments: any }>) {
   const noMenuText = date ? "No menus available for date: " + date : "No menus available today";
 
   const splitData = data?.slice(0, 2);
-  const photos = splitData
-    ?.map((item: any) => (item?.photo ? `![](${item.photo})` : ""))
-    ?.join("\n\n");
+  const photos = splitData?.map((item: IMenuItem) => (item?.photo ? `![](${item.photo})` : ""))?.join("\n\n");
   const markdown = isLoading ? loadingText : photos || noMenuText;
 
   return (
@@ -20,10 +19,8 @@ export default function Command(props: LaunchProps<{ arguments: any }>) {
       markdown={markdown}
       metadata={
         <Detail.Metadata>
-          {splitData?.map((item: any, index: number) => (
-            <>
-              <Detail.Metadata.Label key={index} title={item.name} text={item.menu?.name ?? "No menu"} />
-            </>
+          {splitData?.map((item: IMenuItem, index: number) => (
+            <Detail.Metadata.Label key={index} title={item.name} text={item.menu?.name ?? "No menu"} />
           ))}
           <Detail.Metadata.Separator />
           <Detail.Metadata.Link title="Lunch" target="https://app.kanpla.io/app" text="Sign up in Kanpla" />
--- kanpla/src/utils.ts
+++ kanpla/src/utils.ts
@@ -1,4 +1,6 @@
-export function formatMarkdown(item: any) {
+import { IMenuItem } from "@taulo1999/kanpla-api";
+
+export function formatMarkdown(item: IMenuItem) {
   const menu = item?.menu?.name ?? "No menu";
   const photo = item?.photo ? `![](${item.photo}&w=300&h=200&fit=crop)` : "";
 
