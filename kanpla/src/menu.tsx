d4e92bb4c99d2ad9f6ffd29ee39ecbf320cdc1b5
Thomas Taulo <taulo@live.dk>
2026-04-04 12:18:17 +0200

    update no men text

--- kanpla/src/menu.tsx
+++ kanpla/src/menu.tsx
@@ -9,40 +9,46 @@ export default function Command(props: LaunchProps<{ arguments: { date: string }
   const { isLoading, data } = date ? kanpla.getMenusByDate(new Date(date)) : kanpla.getTodayMenu();
   const onlyWithMenus = data?.filter((item: IMenuItem) => item.menu && item.menu.name) ?? [];
 
+  const noMenuText = date ? "No menus found for this date" : "No menus found for today";
+
   return (
     <List isLoading={isLoading} isShowingDetail={onlyWithMenus.length > 0}>
       {onlyWithMenus.length === 0 ? (
-        <List.EmptyView title="No menus found" />
+        <List.EmptyView title={noMenuText} />
       ) : (
-        onlyWithMenus.map((item: IMenuItem, index: number) => (
-          <List.Item
-            key={index}
-            title={item.menu?.name ?? "No menu"}
-            subtitle={item.name}
-            detail={
-              <List.Item.Detail
-                markdown={item.photo ? `![](${item.photo})` : ""}
-                metadata={
-                  <List.Item.Detail.Metadata>
-                    <List.Item.Detail.Metadata.Label title="Dish" text={item.menu?.name ?? "No menu"} />
-                    <List.Item.Detail.Metadata.Label title="Category" text={item.name} />
-                    <List.Item.Detail.Metadata.Separator />
-                    <List.Item.Detail.Metadata.Link
-                      title="Lunch"
-                      target="https://app.kanpla.io/app"
-                      text="Sign up in Kanpla"
-                    />
-                  </List.Item.Detail.Metadata>
-                }
-              />
-            }
-            actions={
-              <ActionPanel>
-                <Action.OpenInBrowser title="Sign up in Kanpla" url="https://app.kanpla.io/app" />
-              </ActionPanel>
-            }
-          />
-        ))
+        onlyWithMenus.map((item: IMenuItem, index: number) => {
+          const menuName = item.menu?.name ?? "No menu";
+
+          return (
+            <List.Item
+              key={index}
+              title={menuName}
+              subtitle={item.name}
+              detail={
+                <List.Item.Detail
+                  markdown={item.photo ? `![](${item.photo})` : ""}
+                  metadata={
+                    <List.Item.Detail.Metadata>
+                      <List.Item.Detail.Metadata.Label title="Dish" text={menuName} />
+                      <List.Item.Detail.Metadata.Label title="Category" text={item.name} />
+                      <List.Item.Detail.Metadata.Separator />
+                      <List.Item.Detail.Metadata.Link
+                        title="Lunch"
+                        target="https://app.kanpla.io/app"
+                        text="Sign up in Kanpla"
+                      />
+                    </List.Item.Detail.Metadata>
+                  }
+                />
+              }
+              actions={
+                <ActionPanel>
+                  <Action.OpenInBrowser title="Sign up in Kanpla" url="https://app.kanpla.io/app" />
+                </ActionPanel>
+              }
+            />
+          );
+        })
       )}
     </List>
   );
